cf50f66b9a7161393d33320649739f607c1b1957
Abra24-Ro <robinsonabra24@gmail.com>
2025-11-02 21:33:35 -0500

    termino

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,10 @@
 import react from "@vitejs/plugin-react-swc";
 import path from "path";
 import tailwindcss from "@tailwindcss/vite";
-
 import { defineConfig } from "vite";
 
-// https://vite.dev/config/
 export default defineConfig({
+  base: "/ChatReactRouter/", 
   plugins: [react(), tailwindcss()],
   resolve: {
     alias: {
